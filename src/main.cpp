96cff75a1cca0b7d95bb30ce6b8a10b232a55efc
rohit3171999 <rohit3171999@gmail.com>
2026-02-11 10:53:53 +0530

    define screen dimensions and initialize Serial
    Added `SCREEN_WIDTH` and `SCREEN_HEIGHT` macros for display configuration.
    Initialized Serial communication at 9600 baud in `setup()`.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+
 void setup() {
+    Serial.begin(9600);
+
 // write your initialization code here
 }
 
